96f8e6a4d6619438e8d03446a21db567bfc964dd
Lev <tuvarevl@mail.ru>
2022-02-08 10:23:30 +0500

    task 3

--- task3/task3_Creation.sql
+++ task3/task3_Creation.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `bank_deposits`;
+CREATE DATABASE IF NOT EXISTS `bank_deposits` ;
 USE `bank_deposits`;
 
 CREATE TABLE IF NOT EXISTS `client` (
@@ -7,15 +7,16 @@ CREATE TABLE IF NOT EXISTS `client` (
   `passport_number` text NOT NULL,
   `address` text NOT NULL,
   `telephone` text NOT NULL,
-  KEY `code_client` (`code_client`)
+  PRIMARY KEY (`code_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE IF NOT EXISTS `deposit` (
   `code_deposit` int(11) NOT NULL,
   `name_deposit` text DEFAULT NULL,
   `storage_life` int(11) DEFAULT NULL,
   `rate,_per_annum` text DEFAULT NULL,
-  KEY `code deposit` (`code_deposit`)
+  PRIMARY KEY (`code_deposit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `bank account` (
@@ -25,11 +26,9 @@ CREATE TABLE IF NOT EXISTS `bank account` (
   `account_opening_date` date DEFAULT NULL,
   `account_closing_date` date DEFAULT NULL,
   `amount_deposit` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`№ _account`),
   KEY `FK_bank account_client` (`code_client`),
   KEY `FK_bank account_deposit` (`code_deposit`),
-  KEY `№ _account` (`№ _account`),
   CONSTRAINT `FK_bank account_client` FOREIGN KEY (`code_client`) REFERENCES `client` (`code_client`),
   CONSTRAINT `FK_bank account_deposit` FOREIGN KEY (`code_deposit`) REFERENCES `deposit` (`code_deposit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
--- task3/task3_Filling.sql
+++ task3/task3_Filling.sql
@@ -1,5 +1,4 @@
-
-CREATE DATABASE IF NOT EXISTS `bank_deposits`;
+CREATE DATABASE IF NOT EXISTS `bank_deposits` ;
 USE `bank_deposits`;
 
 CREATE TABLE IF NOT EXISTS `client` (
@@ -8,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   `passport_number` text NOT NULL,
   `address` text NOT NULL,
   `telephone` text NOT NULL,
-  KEY `code_client` (`code_client`)
+  PRIMARY KEY (`code_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `client` (`code_client`, `fullname_client`, `passport_number`, `address`, `telephone`) VALUES
@@ -21,15 +20,15 @@ CREATE TABLE IF NOT EXISTS `deposit` (
   `name_deposit` text DEFAULT NULL,
   `storage_life` int(11) DEFAULT NULL,
   `rate,_per_annum` text DEFAULT NULL,
-  KEY `code deposit` (`code_deposit`)
+  PRIMARY KEY (`code_deposit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `deposit` (`code_deposit`, `name_deposit`, `storage_life`, `rate,_per_annum`) VALUES
+	(1, 'Накопительный (в российских рублях)', 13, '10%'),
 	(2, 'Капитал (в долларах США)', 18, '7%'),
 	(3, 'Победа (в белорусских рублях)', 13, 'Ставка рефинансирования +2%'),
 	(4, 'К отпуску (в белорусских рублях)', 12, 'Ставка рефинансирования -1%'),
-	(5, 'Накопительный (в белорусских рублях)', 36, '18%'),
-	(1, 'Накопительный (в российских рублях)', 13, '10%');
+	(5, 'Накопительный (в белорусских рублях)', 36, '18%');
 
 CREATE TABLE IF NOT EXISTS `bank account` (
   `№ _account` int(11) NOT NULL,
@@ -38,9 +37,9 @@ CREATE TABLE IF NOT EXISTS `bank account` (
   `account_opening_date` date DEFAULT NULL,
   `account_closing_date` date DEFAULT NULL,
   `amount_deposit` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`№ _account`),
   KEY `FK_bank account_client` (`code_client`),
   KEY `FK_bank account_deposit` (`code_deposit`),
-  KEY `№ _account` (`№ _account`),
   CONSTRAINT `FK_bank account_client` FOREIGN KEY (`code_client`) REFERENCES `client` (`code_client`),
   CONSTRAINT `FK_bank account_deposit` FOREIGN KEY (`code_deposit`) REFERENCES `deposit` (`code_deposit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -50,5 +49,3 @@ INSERT INTO `bank account` (`№ _account`, `code_client`, `code_deposit`, `acco
 	(2, 2, 1, '2022-01-28', '2022-02-01', 2500),
 	(3, 3, 3, '2022-01-28', '2022-01-29', 42342),
 	(4, 3, 5, '2022-01-27', '2022-02-01', 100000);
-
-
